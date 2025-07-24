7a7e547c5294c628fc4c8a95f233c85dc9fda594
EugeneZakhryapin <postpunk47@gmail.com>
2025-07-24 17:56:16 +0300

    Created Gradle project; added Eureka server and a service for receiving sensor events

---
+++ secrets.example.h
@@ -0,0 +1,10 @@
+//Example of secrets
+#ifndef STASSID
+#define STASSID "WIFI_ID"
+#define STAPSK "WIFI_PASSWORD"
+#endif
+
+#ifndef MQTT_USER
+#define MQTT_USER "MQTT_USER"
+#define MQTT_PASS "MQTT_PASS"
+#endif
