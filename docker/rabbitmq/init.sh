cefeb8840730851dd1743983ddf637ad44165474
EugeneZakhryapin <postpunk47@gmail.com>
2025-07-31 22:19:22 +0300

    Fixed definitions.json import — now done via script [5]

--- docker/rabbitmq/init.sh
+++ docker/rabbitmq/init.sh
@@ -2,7 +2,7 @@
 
 (
   sleep 10;
-  rabbitmqctl import_definitions /definitions.json;
+  rabbitmqctl import_definitions /etc/rabbitmq/definitions.json;
   echo "*** Definitions imported ***"
 ) &
 # Запускаем основной процесс
