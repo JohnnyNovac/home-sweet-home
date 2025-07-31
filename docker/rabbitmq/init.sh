d01acc4dfb288bfb2e2ae24f72202bfd7d9ad45b
EugeneZakhryapin <postpunk47@gmail.com>
2025-07-31 23:02:20 +0300

    Fixed definitions.json import — now done via script [9]

--- docker/rabbitmq/init.sh
+++ docker/rabbitmq/init.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 (
+  TIMEOUT=300  # Timeout in seconds
+  ELAPSED=0
+  INTERVAL=2
+
+  # Wait until port 5672 is open or timeout reached
   echo "Waiting for RabbitMQ to start..."
   until nc -z localhost 5672; do
-    sleep 2
+    if [ "$ELAPSED" -ge "$TIMEOUT" ]; then
+      echo "Timeout reached while waiting for RabbitMQ to start."
+      exit 1
+    fi
+    sleep $INTERVAL
+    ELAPSED=$((ELAPSED + INTERVAL))
   done
 
+  # Import definitions after RabbitMQ is ready
   rabbitmqctl import_definitions /etc/rabbitmq/definitions.json
   echo "*** Definitions imported ***"
 ) &
-# Запускаем основной процесс
+# Running main process
 rabbitmq-server
