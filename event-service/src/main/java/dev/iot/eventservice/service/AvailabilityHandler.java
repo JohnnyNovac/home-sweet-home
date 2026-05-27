ee2fa33475eb7efa99a7a616d98e8902a26035c8
EugeneZakhryapin <postpunk47@gmail.com>
2026-05-27 03:03:38 +0300

    Added dead-letter queues

--- event-service/src/main/java/dev/iot/eventservice/service/AvailabilityHandler.java
+++ event-service/src/main/java/dev/iot/eventservice/service/AvailabilityHandler.java
@@ -2,6 +2,7 @@ package dev.iot.eventservice.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -25,7 +26,13 @@ public class AvailabilityHandler {
 
     @RabbitListener(queues = "${app.rabbitmq.device-availability-queue}")
     public void handle(String body, @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
-        String deviceId = parseDeviceId(routingKey);
+        String deviceId;
+        try {
+            deviceId = parseDeviceId(routingKey);
+        } catch (IllegalArgumentException e) {
+            logger.error("Discarding availability message with bad routing key: {}", routingKey, e);
+            throw new AmqpRejectAndDontRequeueException("Unprocessable availability message: " + routingKey, e);
+        }
         logger.info("Device {} is {}", deviceId, body);
 
         deviceRegistry.recordSeen(deviceId, null)
